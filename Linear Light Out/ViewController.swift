f2eafaf2c493f6a80c8c3c13fc6ce06bca005f20
sunh5 <sunh5@rose-hulman.edu>
2020-12-16 15:40:44 -0500

    Initial Commit

---
+++ Linear Light Out/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Linear Light Out
+//
+//  Created by Haoxuan Sun on 12/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
